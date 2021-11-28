e4ee0d3195829fe1064d3fb8aa80ab28ef1c061d
Leszek Słowikowski <lslowikowski@gmail.com>
2021-11-28 13:09:24 +0100

    ver 002 - dodanie stałych oraz dokumentacji technicznej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,27 @@ package com.company;
 
 import java.util.Scanner;
 
+/********************************************************
+ * nazwa klasy: Main
+ * atrybuty klasy: LICZBA_MIESIACY - liczba miesięcy, w których rejestrujemy zużycia
+ *                  CENA_1_KW - cena wody w 1 kwartale
+ *                  CENA_R_KW  - cena wody w pozostałych kwartałach roku
+ *                  zuzycia - tablica przechowująca zużycia
+ * opis: główna klasa uruchomienowa
+ * autor: 67890123456
+ * ****************************************************/
 public class Main {
     final static int LICZBA_MIESIACY = 12;
-    final static float cena1Kwartal = 6.0F;
-    final static float cenaRKwartal = 7.0F;
+    final static float CENA_1_KW = 6.0F;
+    final static float CENA_R_KW = 7.0F;
     static float[] zuzycia = new float[LICZBA_MIESIACY];
 
+    /********************************************************
+     * nazwa funkcji: main
+     * parametry wejściowe: args - parametry przekazywane podczas uruchomienia
+     * wartość zwracana: brak
+     * autor: 67890123456
+     * ****************************************************/
     public static void main(String[] args) {
         Scanner buforKlawiatury = new Scanner(System.in);
         for (int i = 0; i < LICZBA_MIESIACY ; i++) {
@@ -25,7 +40,7 @@ public class Main {
 
         float srednieZuzycie = zuzyciaWody.obliczSrednieZuzycie();
 
-        float rocznyKosztZuzycia = zuzyciaWody.obliczCene(cena1Kwartal, cenaRKwartal);
+        float rocznyKosztZuzycia = zuzyciaWody.obliczCene(CENA_1_KW, CENA_R_KW);
 
         System.out.println("W miesiącu " + miesiacMinZuzycia + " zużyto najmniej wody: " + minZuzycie);
         System.out.println("W miesiącu " + miesiacMaxZuzycia + " zużyto najwięcej wody: " + maxZuzycie);
--- src/com/company/ZuzyciaWody.java
+++ src/com/company/ZuzyciaWody.java
@@ -1,14 +1,33 @@
 package com.company;
-
+/********************************************************
+ * nazwa klasy: ZuzyciaWody
+ * atrybuty klasy:  zuzycia - tablica przechowująca zużycia
+ *                  iloscMiesiecy - liczba miesięcy, w których rejestrujemy zużycia
+ * opis: Klasa wykonująca operacje na zużyciach wody
+ * autor: 67890123456
+ * ****************************************************/
 public class ZuzyciaWody {
     private float[] zuzycia;
     private int iloscMiesiecy;
 
+    /********************************************************
+     * nazwa funkcji: ZuzyciaWody - konstruktor
+     * parametry wejściowe: zuzycia - tablica przechowująca zużycia
+     *                      iloscMiesiecy - liczba miesięcy, w których rejestrujemy zużycia
+     * wartość zwracana: brak
+     * autor: 67890123456
+     * ****************************************************/
     public ZuzyciaWody(float[] zuzycia, int iloscMiesiecy) {
         this.iloscMiesiecy = iloscMiesiecy;
         this.zuzycia = zuzycia;
     }
 
+    /********************************************************
+     * nazwa funkcji: znajdzMiesiacMinZuzycia
+     * parametry wejściowe: brak
+     * wartość zwracana: miesiąc, w którym było najmniejsze zużycie wody
+     * autor: 67890123456
+     * ****************************************************/
     public int znajdzMiesiacMinZuzycia(){
         int miesiacMin = 0;
         for (int i = 0; i < iloscMiesiecy; i++) {
@@ -19,6 +38,12 @@ public class ZuzyciaWody {
         return miesiacMin;
     }
 
+    /********************************************************
+     * nazwa funkcji: znajdzMiesiacMaxZuzycia
+     * parametry wejściowe: brak
+     * wartość zwracana: miesiąc, w którym było największe zużycie wody
+     * autor: 67890123456
+     * ****************************************************/
     public int znajdzMiesiacMaxZuzycia(){
         int miesiacMax = 0;
         for (int i = 0; i < iloscMiesiecy; i++) {
@@ -29,6 +54,12 @@ public class ZuzyciaWody {
         return miesiacMax;
     }
 
+    /********************************************************
+     * nazwa funkcji: obliczSrednieZuzycie
+     * parametry wejściowe: brak
+     * wartość zwracana: średnie roczne zużycie wody
+     * autor: 67890123456
+     * ****************************************************/
     public float obliczSrednieZuzycie(){
         float suma = 0;
         for (int i = 0; i <iloscMiesiecy ; i++) {
@@ -39,6 +70,13 @@ public class ZuzyciaWody {
         return suma / iloscMiesiecy;
     }
 
+    /********************************************************
+     * nazwa funkcji: obliczCzene
+     * parametry wejściowe: cena1Kw - cena wody w pierwszym kwartale
+     *                      cena1Kw - cena wody w pozostałych kwartałach roku
+     * wartość zwracana: Oblicza koszt zużycia wody za cały rok
+     * autor: 67890123456
+     * ****************************************************/
     public float obliczCene( float cena1Kw, float cenaRKw){
         float koszt1Kw = 0;
         for (int i = 0; i < 3 ; i++) {
